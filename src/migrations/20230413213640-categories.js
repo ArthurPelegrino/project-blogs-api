6dba8767dbd7d020930642b683f609084470397e
Arthur Pelegrino <arthurpelegrino97@gmail.com>
2023-04-13 21:48:00 -0300

    Configurando as migrations

---
+++ src/migrations/20230413213640-categories.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const sequelize = require('sequelize')
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('categories', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('categories');
+  }
+};
